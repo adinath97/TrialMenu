da5470380bdc13066252119fcab079ffb672d969
adinath97 <58251104+adinath97@users.noreply.github.com>
2020-07-17 23:50:02 -0700

    Add files via upload

---
+++ TrialMenu/index.js
@@ -0,0 +1,39 @@
+function dropIt() {
+    document.getElementById("dropdown").classList.toggle("display-content");
+    dropIt1A();
+    dropIt2A();
+    dropIt1B()
+  }
+
+function dropIt1A() {
+    document.getElementById("dropdown1A").classList.toggle("display-content");
+}
+
+function dropIt2A() {
+    document.getElementById("dropdown2A").classList.toggle("display-content");
+}
+
+function dropIt1B() {
+    document.getElementById("dropdown1B").classList.toggle("display-content");
+}
+
+function rotate() {
+    document.getElementById("triangleIcon").classList.toggle("rotateIcon");
+}
+
+function rotate1A() {
+    document.getElementById("triangleIcon1A").classList.toggle("rotateIcon");
+}
+
+function rotate2A() {
+    document.getElementById("triangleIcon2A").classList.toggle("rotateIcon");
+}
+
+function rotate1B() {
+    document.getElementById("triangleIcon1B").classList.toggle("rotateIcon");
+}
+
+function rotate2B() {
+    document.getElementById("triangleIcon2B").classList.toggle("rotateIcon");
+}
+
